48ac88d6e583f9f5375802def106982c4433a2c1
Adrina-peighambarzadeh <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-22 20:23:25 -0400

    ..

--- answer.php
+++ answer.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta name="description" content="Volume of a Sphere, with a Query String" />
   <meta name="keywords" content="mths, icd2o" />
-  <meta name="author" content="Ain Jeong" />
+  <meta name="author" content="Adrina peighambarzadrh" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="./css/style.css" />
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
